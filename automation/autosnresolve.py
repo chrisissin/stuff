597d59fcdf2963ba35cb295591d6e9abacf3f3b2
chrisho <chrisissin@gmail.com>
2021-09-14 09:42:56 -0700

    Create autosnresolve.py

---
+++ automation/autosnresolve.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+pyautogui.click(437,710)
+pyautogui.click(437,710)
+time.sleep(2)
+pyautogui.click(1135,421)
+pyautogui.click(1123,577)
+time.sleep(1)
+pyautogui.click(1094,176)
