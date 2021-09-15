38b11d440d91c90212c334bc2915b6358944f45e
chrisho <chrisissin@gmail.com>
2021-09-15 11:13:50 -0700

    Update autosnresolve.py

--- automation/autosnresolve.py
+++ automation/autosnresolve.py
@@ -1,9 +1,15 @@
 import pyautogui
 import time
-pyautogui.click(437,710)
-pyautogui.click(437,710)
-time.sleep(2)
-pyautogui.click(1135,421)
-pyautogui.click(1123,577)
-time.sleep(1)
-pyautogui.click(1094,176)
+for x in range(5):
+	pyautogui.click(437,710)
+	pyautogui.click(437,710)
+	time.sleep(2)
+	pyautogui.click(1135,421)
+	pyautogui.click(1123,577)
+	time.sleep(1)
+	pyautogui.click(1094,176)
+	time.sleep(3)
+	pyautogui.click(1222,674)
+	time.sleep(1)
+	pyautogui.click(1222,674)
+	time.sleep(3)
